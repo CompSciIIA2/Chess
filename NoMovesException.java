473b0637e8ef839ebe54f3b8101884b741c48e87
Michael Nichols <mrn.alnitak@gmail.com>
2015-07-01 01:19:48 -0500

    Squashed 'src/mrnichols/alphabeta/' content from commit a5bd9c5
    git-subtree-dir: src/mrnichols/alphabeta
    git-subtree-split: a5bd9c507969eee2b0807039114fa813ea74b577

---
+++ NoMovesException.java
@@ -0,0 +1,11 @@
+package mrnichols.alphabeta;
+
+/**
+ * Exception in which no legal moves exist in the alpha-beta 
+ * pruned minimax algorithm.
+ * 
+ * @author Michael Nichols
+ * @version 1.0
+ */
+@SuppressWarnings("serial")
+public class NoMovesException extends Exception { }
